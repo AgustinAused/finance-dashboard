1a99f3f145b62e89b8377ccc47d467d5abe1920d
Agustin Aused <agus.aused@gmail.com>
2025-01-19 09:40:11 -0300

    Add financial charts to dashboard: implement loading state, fetch user profile, and update layout metadata

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)', // Aplica a todas las rutas
+            headers: [
+              { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+              { key: 'Pragma', value: 'no-cache' },
+              { key: 'Expires', value: '0' },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,12 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { getCashFlowDefault } from '@/api/FinancialApi'; // Importa la API
+import { getProfile } from '@/api/UserApi';
 import FinancialSummary from '@/components/dashboard/FinancialSummary'; // Importa el componente
+import FinancialCharts from '@/components/dashboard/FinancialCharts'; // Importa el componente de gráficos
+import CircularProgress from '@mui/material/CircularProgress'; // Importa CircularProgress de MUI
+import Box from '@mui/material/Box'; // Importa Box para centrar el loader
+
 
 function Dashboard() {
   const [finances, setFinances] = useState({ income: 0, expenses: 0, netCashFlow: 0 });
@@ -9,11 +14,12 @@ function Dashboard() {
   const [selectedOption, setSelectedOption] = useState(null); // Opción de mes o trimestre seleccionada
   const [year, setYear] = useState(new Date().getFullYear()); // Año actual como valor predeterminado
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // Estado de carga
 
   useEffect(() => {
     const fetchData = async () => {
       let periodValue = year; 
-  
+
       // Verifica si el periodo es mensual o trimestral, y agrega la opción seleccionada
       if (selectedPeriod === 'monthly' && selectedOption) {
         periodValue = `${selectedOption} ${year}`; 
@@ -22,18 +28,45 @@ function Dashboard() {
       } else if (selectedPeriod === 'anual') {
         periodValue = `${year}`; 
       }
-  
+
       try {
-        const data = await getCashFlowDefault(periodValue); 
+        const data = await getCashFlowDefault(periodValue);
+        const userData = await getProfile();
+        
+        // Verifica si userData y userData.data existen
+        if (userData && userData.data) {
+          setUser(userData.data);
+        } else {
+          console.error('User data not found');
+        }
+
         setFinances(data.data); 
+
       } catch (error) {
         console.error('Error fetching financial data', error);
+      } finally {
+        setLoading(false); // Al finalizar, actualiza el estado de carga
       }
     };
-  
+
     fetchData();
   }, [selectedPeriod, selectedOption, year]); // Dependemos del periodo, la opción y el año
-  
+
+  // Si los datos están siendo cargados, muestra el loader de MUI
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   return (
     <div>
@@ -46,6 +79,7 @@ function Dashboard() {
         setYear={setYear}
         year={year}
       />
+      <FinancialCharts companyId={user.company.id} />
     </div>
   );
 }
