9c88d8cd4afe87514b27d598b1a873b538dd3729
Agustin Aused <agus.aused@gmail.com>
2025-01-21 17:31:43 -0300

    Add password change modal to Dashboard and implement profile editing in ProfilePage

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,74 +1,80 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { getCashFlowDefault } from '@/api/FinancialApi'; // Importa la API
-import { getProfile } from '@/api/UserApi';
+import { getProfile, changePassword } from '@/api/UserApi'; // Importa la API para cambiar la contraseña
 import FinancialSummary from '@/components/dashboard/FinancialSummary'; // Importa el componente
 import FinancialCharts from '@/components/dashboard/FinancialCharts'; // Importa el componente de gráficos
 import CircularProgress from '@mui/material/CircularProgress'; // Importa CircularProgress de MUI
 import Box from '@mui/material/Box'; // Importa Box para centrar el loader
+import Modal from '@mui/material/Modal'; // Importa Modal de MUI
+import TextField from '@mui/material/TextField'; // Importa TextField para el formulario
+import Button from '@mui/material/Button'; // Importa Button de MUI
 
-
-function Dashboard() {
+export default function Dashboard() {
   const [finances, setFinances] = useState({ income: 0, expenses: 0, netCashFlow: 0 });
   const [selectedPeriod, setSelectedPeriod] = useState('anual'); // Periodo inicial (anual)
   const [selectedOption, setSelectedOption] = useState(null); // Opción de mes o trimestre seleccionada
   const [year, setYear] = useState(new Date().getFullYear()); // Año actual como valor predeterminado
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true); // Estado de carga
+  const [showModal, setShowModal] = useState(false); // Estado para mostrar el modal
+  const [newPassword, setNewPassword] = useState(''); // Nuevo valor de la contraseña
+  const [errorMessage, setErrorMessage] = useState(''); // Mensaje de error
 
   useEffect(() => {
     const fetchData = async () => {
-      let periodValue = year; 
-  
+      let periodValue = year;
+
       // Construye el valor del periodo según la selección
       if (selectedPeriod === 'monthly' && selectedOption) {
-        periodValue = `${selectedOption} ${year}`; 
+        periodValue = `${selectedOption} ${year}`;
       } else if (selectedPeriod === 'quarterly' && selectedOption) {
-        periodValue = `${selectedOption} ${year}`; 
+        periodValue = `${selectedOption} ${year}`;
       } else if (selectedPeriod === 'anual') {
-        periodValue = `${year}`; 
+        periodValue = `${year}`;
       }
-  
+
       try {
         const cashflowData = await getCashFlowDefault(periodValue);
         const userData = await getProfile();
-  
+
         // Asegura que los datos sean válidos antes de actualizar el estado
         if (userData && userData.data) {
-          console.log(userData)
           setUser(userData.data);
-        } else {
-          console.error('User data not found');
+
+          // Si el usuario está inactivo, mostrar el modal
+          if (!userData.data.active) {
+            setShowModal(true); // Mostrar el modal si el usuario no está activo
+          }
         }
-  
+
         // Aquí, si la respuesta de la API no es válida, mantiene los datos anteriores
         if (cashflowData && cashflowData.data) {
           setFinances(cashflowData.data);
         } else {
-          // console.error('Financial data not found');
-          // Mantener los datos anteriores en caso de error
-          setFinances(prevFinances => ({
-            ...prevFinances, 
-          }));
+          setFinances(prevFinances => ({ ...prevFinances }));
         }
-  
+
       } catch (error) {
-        setError(true);
-        // En caso de error, asegurarse de mantener los datos anteriores
-        setFinances(prevFinances => ({
-          ...prevFinances, 
-        }));
-        // console.error('Error fetching financial data', error);
+        setErrorMessage('Error fetching data');
+        setFinances(prevFinances => ({ ...prevFinances }));
       } finally {
         setLoading(false); // Al finalizar, actualiza el estado de carga
       }
     };
 
-    // Si no hay cambios significativos, no ejecutar la llamada
-  if (selectedPeriod && year && (!selectedOption || selectedOption)) {
     fetchData();
-  }
-  }, [selectedPeriod, selectedOption, year]); // Dependemos del periodo, la opción y el año
+  }, [selectedPeriod, selectedOption, year]);
+
+  const handlePasswordChange = async () => {
+    try {
+      // Enviar la nueva contraseña al backend
+      await changePassword(newPassword);
+      setShowModal(false); // Cerrar el modal tras el cambio
+    } catch (error) {
+      setErrorMessage('Error al cambiar la contraseña');
+    }
+  };
 
   // Si los datos están siendo cargados, muestra el loader de MUI
   if (loading) {
@@ -98,8 +104,58 @@ function Dashboard() {
         year={year}
       />
       <FinancialCharts companyId={user.company.id} />
+
+      {/* Modal de cambio de contraseña */}
+      <Modal
+        open={showModal}
+        onClose={() => { } /* No hacemos nada aquí para prevenir el cierre */}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        disableBackdropClick // Deshabilita el cierre al hacer clic fuera del modal
+        disableEscapeKeyDown // Deshabilita el cierre al presionar la tecla "Esc"
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            boxShadow: 24,
+            padding: 3,
+            maxWidth: 400,
+            margin: 'auto',
+            backgroundColor: 'white',
+            borderRadius: 2,
+          }}
+        >
+          <div>
+            <h2 id="modal-title">Cambiar por primer ingreso </h2>
+            <TextField
+              label="Nueva Contraseña"
+              type="password"
+              fullWidth
+              variant="outlined"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              sx={{ marginBottom: 2 }}
+            />
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={handlePasswordChange}
+            >
+              Cambiar Contraseña
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }
 
-export default Dashboard;
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,17 +1,20 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import ProfileHeader from "@/components/profile/Header";
-import CircularProgress from "@mui/material/CircularProgress"; // Importar el loader de MUI
-import { getProfile } from "@/api/UserApi";
+import CircularProgress from "@mui/material/CircularProgress";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import EditForm from "@/components/profile/EditForm";
+import { getProfile, updateProfile } from "@/api/UserApi";
 
 export default function ProfilePage() {
     const [user, setUser] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         const fetchUserProfile = async () => {
             const userData = await getProfile();
             if (userData && userData.data) {
-                console.log(userData);
                 setUser(userData.data);
             } else {
                 console.error("User data not found");
@@ -22,8 +25,17 @@ export default function ProfilePage() {
     }, []);
 
     const handleEditClick = () => {
-        // Lógica para editar el perfil
-        console.log("Editar perfil");
+        setOpenModal(true); // Abrir el modal
+    };
+
+    const handleSaveChanges = async (updatedUser) => {
+        const response = await updateProfile(updatedUser);
+        if (response && response.status === "success") {
+            setUser(updatedUser); // Actualizar el estado con los cambios
+            setOpenModal(false); // Cerrar el modal
+        } else {
+            console.error("Error al actualizar el perfil");
+        }
     };
 
     if (!user) {
@@ -44,6 +56,27 @@ export default function ProfilePage() {
     return (
         <div className="profile-page">
             <ProfileHeader user={user} onEditClick={handleEditClick} />
+
+            {/* Modal para editar perfil */}
+            <Modal open={openModal} onClose={() => setOpenModal(false)}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: 400,
+                        boxShadow: 24,
+                        p: 4,
+                        borderRadius: 2,
+                    }}
+                >
+                    <EditForm
+                        user={user}
+                        onSave={handleSaveChanges}
+                    />
+                </Box>
+            </Modal>
         </div>
     );
 }
